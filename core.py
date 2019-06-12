ebd1e84bc9f4df3ce7b11482cfed37d0cbdfa0bb
pjl <821216100@qq.com>
2019-06-12 21:26:40 +0800

    add comment

--- core.py
+++ core.py
@@ -10,11 +10,13 @@ class Core:
     self.__now_state = [[False for x in range(c)] for i in range(r)]
     self.__next_state = [[False for x in range(c)] for i in range(r)]
     
+  #变更第r行c列细胞的状态
   def change_block(self,r,c):
     try:
       self.__now_state[r][c]^=1
     except IndexError:pass
-    
+  
+  #获取第r行c列细胞周围存活细胞的个数
   def __get_live_num(self,r, c):
     sum=0
     try:
@@ -43,6 +45,7 @@ class Core:
     except IndexError:pass
     return sum
   
+  #获取下一个时刻所有细胞的存活状态
   def get_next_state(self):
     for r in range(len(self.__now_state)):
       for c in range(len(self.__now_state[0])):
@@ -58,6 +61,7 @@ class Core:
     self.__now_state,self.__next_state=self.__next_state,self.__now_state
     return self.__now_state
   
+  #将当前时刻所有细胞设置为死亡
   def clear(self):
     for r in range(len(self.__now_state)):
       for c in range(len(self.__now_state[0])):
