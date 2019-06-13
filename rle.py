fc37ab5ca4cd2d33cdcce399070d559904a2add0
pjl <821216100@qq.com>
2019-06-13 12:09:56 +0800

    add glider_generater

---
+++ rle.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 10:43:40 2019
+
+@author: pjl
+"""
+
+glider= [[False,False,True],
+      [True,False,True],
+      [False,True,True]]
+blinker= [[False,True,False],
+      [False,True,False],
+      [False,True,False]]
+toad=[[False,False,False,False],
+        [False,True,True,True],
+        [True,True,True,False],
+        [False,False,False,False]]
+HWSS= [[False,False,False,True,True,False,False],
+        [False,True,False,False,False,False,True],
+        [True,False,False,False,False,False,False],
+        [True,False,False,False,False,False,True],
+        [True,True,True,True,True,True,False],
+        [False,False,False,False,False,False,False],
+        [False,False,False,False,False,False,False]]
+glider_generater=[
+    [False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,True,True,False,False,False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,True,True,True,False,False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False,True,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,True,True,False,True,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,True,False,True,False,False,False,False,True,True,True,False,False,False,False,False,False,False,False,True,False,False,True,False,False,False,False,False,True,True],
+    [False,False,False,False,False,False,True,False,True,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,True,True,False,True,False,False,False,False,False,True,True],
+    [True,True,False,False,False,True,False,False,True,False,False,False,False,False,False,False,True,False,False,True,True,False,False,True,True,True,False,False,False,False,False,False,False,False,False,False],
+    [True,True,False,False,False,False,True,False,True,False,False,False,False,False,False,False,True,False,False,False,True,False,False,True,True,False,False,False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,True,False,True,False,False,False,False,False,False,True,False,False,True,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False],
+    [False,False,False,False,False,False,False,False,False,True,False,False,False,False,False,False,False,False,True,True,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+    ]
